ff622819015cd548f5aac87a1719ff3984a4866f
DePrincio <princetlive@yahoo.com>
2021-05-25 14:25:05 -0400

    annotate the return type

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,14 +4,17 @@ interface RespositoriesState {
 	data: string[];
 }
 
-const reducer = (state: RespositoriesState, action: any) => {
+const reducer = (
+	state: RespositoriesState,
+	action: any
+): RespositoriesState => {
 	switch (action.type) {
 		case 'search_repositories':
 			return { loading: true, error: null, data: [] };
 		case 'search_repositories_success':
 			return { loading: false, error: null, data: action.payload };
 		case 'search_repositories_error':
-			return { loadig: false, error: action.payload, data: [] };
+			return { loading: false, error: action.payload, data: [] };
 		default:
 			return state;
 	}
