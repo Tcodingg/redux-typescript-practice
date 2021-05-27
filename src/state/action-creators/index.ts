4b84c2db62d038d102a5659bc76b6f5a2e1bc3c2
DePrincio <princetlive@yahoo.com>
2021-05-27 13:04:32 -0400

    add request logic

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -2,11 +2,26 @@ import axios from 'axios';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 
-const searchRepositories = (term: string) => async (dispatch: any) => {
+export const searchRepositories = (term: string) => async (dispatch: any) => {
 	dispatch({
 		type: ActionType.SEARCH_REPOSITORIES,
 	});
 	try {
+		const { data } = await axios.get(
+			'https://registery.npmjs.org/-/v1/search',
+			{
+				params: {
+					text: term,
+				},
+			}
+		);
+		const names = data.objects.map((result: any) => {
+			return result.package.name;
+		});
+		dispatch({
+			type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+			payload: names,
+		});
 	} catch (err) {
 		dispatch({
 			type: ActionType.SEARCH_REPOSITORIES_ERROR,
