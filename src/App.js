2a3b727c3804022e8c4f98eac7ec7bb3aae7ef07
DePrincio <princetlive@yahoo.com>
2021-05-27 14:36:54 -0400

    wire up to react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../this.state.';
+import RepositoriesList from './RepositoriesList';
 
 export default function App() {
-	return <div></div>;
+	return (
+		<Provider store={store}>
+			<div>
+				<h1>Search For a Pachage</h1>
+				<RepositoriesList />
+			</div>
+		</Provider>
+	);
 }
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,31 +1,33 @@
 import axios from 'axios';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
+import { Dispatch } from 'redux';
 
-export const searchRepositories = (term: string) => async (dispatch: any) => {
-	dispatch({
-		type: ActionType.SEARCH_REPOSITORIES,
-	});
-	try {
-		const { data } = await axios.get(
-			'https://registery.npmjs.org/-/v1/search',
-			{
-				params: {
-					text: term,
-				},
-			}
-		);
-		const names = data.objects.map((result: any) => {
-			return result.package.name;
-		});
+export const searchRepositories =
+	(term: string) => async (dispatch: Dispatch<Action>) => {
 		dispatch({
-			type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
-			payload: names,
+			type: ActionType.SEARCH_REPOSITORIES,
 		});
-	} catch (err) {
-		dispatch({
-			type: ActionType.SEARCH_REPOSITORIES_ERROR,
-			payload: err.message,
-		});
-	}
-};
+		try {
+			const { data } = await axios.get(
+				'https://registery.npmjs.org/-/v1/search',
+				{
+					params: {
+						text: term,
+					},
+				}
+			);
+			const names = data.objects.map((result: any) => {
+				return result.package.name;
+			});
+			dispatch({
+				type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+				payload: names,
+			});
+		} catch (err) {
+			dispatch({
+				type: ActionType.SEARCH_REPOSITORIES_ERROR,
+				payload: err.message,
+			});
+		}
+	};
