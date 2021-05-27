ad9fdf8deaffd9b67a80402ed7693fcb06acbd93
DePrincio <princetlive@yahoo.com>
2021-05-27 12:50:36 -0400

    add action creators

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+const searchRepositories = (term: string) => async (dispatch: any) => {
+	dispatch({
+		type: ActionType.SEARCH_REPOSITORIES,
+	});
+	try {
+	} catch (err) {
+		dispatch({
+			type: ActionType.SEARCH_REPOSITORIES_ERROR,
+			payload: err.message,
+		});
+	}
+};
